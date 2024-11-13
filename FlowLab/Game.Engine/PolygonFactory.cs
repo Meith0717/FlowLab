ae5f2fb34941a1a27676c733caa5e6ec16a0a9e8
Thierry Meiers <meith0717@icloud.com>
2024-11-13 16:43:46 +0100

    Add new simulation components

---
+++ FlowLab/Game.Engine/PolygonFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+using MonoGame.Extended.Shapes;
+using System;
+
+namespace FlowLab.Engine
+{
+    public static class PolygonFactory
+    {
+        public static Polygon CreateCircle(double radius, int sides)
+        {
+            Vector2[] array = new Vector2[sides];
+            double num = Math.PI * 2.0 / sides;
+            double num2 = 0.0;
+            for (int i = 0; i < sides; i++)
+            {
+                array[i] = new Vector2((float)(radius * Math.Cos(num2)) + (float)radius, (float)(radius * Math.Sin(num2) + (float)radius));
+                num2 += num;
+            }
+
+            return new(array);
+        }
+
+        public static Polygon CreateRectangle(int widthCount, int heightCount)
+        {
+            var rectangle = new RectangleF(0, 0, widthCount + 1, heightCount + 1);
+            var corners = rectangle.GetCorners();
+            return new(corners);
+        }
+    }
+}
---
+++ FlowLab/Game.Logic/ScenarioManagement/ScenarioManager.cs
@@ -0,0 +1,48 @@
+﻿using FlowLab.Engine;
+using FlowLab.Logic.ParticleManagement;
+using MonoGame.Extended.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlowLab.Logic.ScenarioManagement
+{
+    internal class ScenarioManager
+    {
+        private readonly Dictionary<Polygon, Action> _scenes;
+        private int _activeSceneIndex;
+
+        public ScenarioManager()
+        {
+            _activeSceneIndex -= 1;
+            _scenes = new()
+            {
+                { PolygonFactory.CreateRectangle(40, 40), null },
+                { PolygonFactory.CreateRectangle(5, 50), null },
+                { PolygonFactory.CreateRectangle(170, 130), null },
+                { PolygonFactory.CreateRectangle(170, 170), null },
+                { PolygonFactory.CreateRectangle(250, 150), null },
+                { PolygonFactory.CreateRectangle(250, 250), null },
+                { PolygonFactory.CreateCircle(70, 50), null },
+                { PolygonFactory.CreateCircle(120, 60), null },
+            };
+        }
+
+        public void NextScene(ParticleManager particleManager)
+        {
+            if (_scenes.Count <= 0) return;
+            _activeSceneIndex = (_activeSceneIndex + 1) % _scenes.Count;
+            ApplyScene(_activeSceneIndex, particleManager);
+        }
+
+        private void ApplyScene(int index, ParticleManager particleManager)
+        {
+            var keyValue = _scenes.ElementAt(index);
+            var polygone = keyValue.Key;
+            var action = keyValue.Value;
+            particleManager.ClearAll();
+            particleManager.AddPolygon(polygone);
+            action?.Invoke();
+        }
+    }
+}
