7673322131aa013a86bee62aa37330287cc23ccc
Thierry Meiers <meith0717@icloud.com>
2024-12-12 17:49:51 +0100

    Add empty classes for new scenario management

---
+++ FlowLab/Logic/ScenarioManagement/Body.cs
@@ -0,0 +1,20 @@
+﻿// Body.cs 
+// Copyright (c) 2023-2025 Thierry Meiers 
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowLab.Logic.ScenarioManagement
+{
+    /// <summary>
+    /// A Body can be moved ore rotated and is composed of a set of movable bounday Particles
+    /// </summary>
+    internal class Body
+    {
+
+    }
+}
---
+++ FlowLab/Logic/ScenarioManagement/Scenario.cs
@@ -0,0 +1,20 @@
+﻿// Scenario.cs 
+// Copyright (c) 2023-2025 Thierry Meiers 
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowLab.Logic.ScenarioManagement
+{
+    /// <summary>
+    /// A scenario is construct out of Bodys containing border Particles
+    /// </summary>
+    internal class Scenario
+    {
+
+    }
+}
