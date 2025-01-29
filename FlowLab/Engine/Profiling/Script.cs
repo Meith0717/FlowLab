c1d828263c2bb37ecc7769b21a20685192efb60e
Thierry Meiers <meith0717@icloud.com>
2025-01-30 00:40:43 +0100

    Update experiment script

--- FlowLab/Engine/Profiling/Script.cs
+++ FlowLab/Engine/Profiling/Script.cs
@@ -10,22 +10,28 @@ namespace FlowLab.Engine.Profiling
 {
     internal class Script
     {
-        private float Counter;
+        public const float Threshold = 500;
+        private float _thresholdCounter;
+        private int _counter;
         public bool Active = false;
+        private readonly Random _random = new();
 
         public void Update(SimulationState simulationState, SimulationSettings simulationSettings)
         {
             if (!Active) return;
-            Counter += simulationSettings.TimeStep;
-            if (Counter < 150) return;
-            Counter = Counter - 150;
-            simulationSettings.Gamma1 -= 0.02f;
+            _thresholdCounter += simulationSettings.TimeStep;
+            if (_thresholdCounter < Threshold) return;
+            _thresholdCounter = _thresholdCounter - Threshold;
+            simulationSettings.Gamma1 = _random.Next(75, 125) / 100f;
+            simulationSettings.Gamma2 = _random.Next(75, 125) / 100f;
+            simulationSettings.Gamma3 = _random.Next(75, 125) / 100f;
+            _counter++;
         }
-
+        
         public void BreakCondition(SimulationState simulationState, SimulationSettings simulationSettings, Action breakAction)
         {
             if (!Active) return;
-            if (simulationSettings.Gamma1 > 0.9f) return;
+            if (_counter < 200) return;
             breakAction?.Invoke();
             Active = false;
         }
